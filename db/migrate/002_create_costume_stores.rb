4905406cd89f0b47516fae407275f47e67b48a0f
Daniel Amyakar <dan.rockit@gmail.com>
2020-06-19 13:55:56 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -3,6 +3,12 @@
 class CostumeStores < ActiveRecord::Migration
   def change
     create_table :costume_stores
-      
+      t.sting :name
+      t.sting :location
+      t.integer :number_of_comstumes
+      t.integer :number_of_employees
+      t.bool :in_business
+      t.timestamp :opening_time
+      t.timestamp :closing_time
   end
 end
