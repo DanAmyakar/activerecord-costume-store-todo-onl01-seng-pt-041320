af56c8e5e2dddbfa3d7a348a2b00246092193251
Daniel Amyakar <dan.rockit@gmail.com>
2020-06-19 15:25:56 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -8,8 +8,8 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
       t.string :theme
       t.float :price
       t.boolean :family_friendly
-      t.datetime.new :opening_date
-      t.datetime.new :closing_date
+      t.datetime :opening_date
+      t.datetime :closing_date
       t.text :long_description
     end
   end
